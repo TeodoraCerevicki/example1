c6c51c4b64f770c9e8f08fe81ae73d6f59584bfc
Teodora Cerevicki <git@github.com:TeodoraCerevicki/example1.git>
2020-12-08 20:45:44 +0100

    Working on about page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-function toggleContent() {
-    console.log('mama');
-    $('.js-toggleClick').click(function(e) {
-        e.preventDefault();
-
-        var target = $(this).attr('data-target');
-        $(target).slideToggle();
+$(document).ready(function () {
+    $(function(){
+        console.log('mama');
+        $('.js-toggleClick').click(function(e) {
+            e.preventDefault();
+    
+            var target = $(this).attr('data-target');
+            $(target).slideToggle();
+        });
     });
-}
+});
 
-toggleContent();
+
+
