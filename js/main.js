b7fab267c29ced3e6182ba8d3d0c979596d76521
Teodora Cerevicki <git@github.com:TeodoraCerevicki/example1.git>
2020-12-17 00:16:52 +0100

    created classes page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
     $(function(){
-        console.log('mama');
         $('.js-toggleClick').click(function(e) {
             e.preventDefault();
     
             var target = $(this).attr('data-target');
             $(target).slideToggle();
+        });
+    });
+
+    $(function(){
+        var tabSwitcher = $('.js-tabSwitch');
+        var tabContent = $('.js-tabContent');
+
+        tabContent.hide();
+        tabContent.first().show();
+
+        tabSwitcher.click(function(e) {
+            e.preventDefault();
+
+            tabSwitcher.removeClass('-active');
+            $(this).addClass('-active');
+
+            tabContent.hide();
+            var target = $(this).attr('data-target');
+            $(target).show();
         });
     });
 });
