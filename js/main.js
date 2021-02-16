93bb82cd7acc983ef70326af789331bf12cecec7
Teodora Cerevicki <git@github.com:TeodoraCerevicki/example1.git>
2021-02-16 20:58:43 +0100

    added slider to footer

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
     $('.js-slider').slick({
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         arrows: false
     });
 
